e34ac3b36bc077744464d94cc948b81aecf435de
andytt <tta02303991@gmail.com>
2014-11-02 14:31:14 +0800

    Fix styles.

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Gallery</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="//cdnjs.cloudflare.com/ajax/libs/jquery.colorbox/1.4.33/example4/colorbox.min.css" rel="stylesheet">
