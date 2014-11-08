28c3ab733bc32151e2009069be74ae55356c7715
andytt <tta02303991@gmail.com>
2014-11-08 12:51:39 +0800

    Add guest page background.

--- app/views/layouts/guest.blade.php
+++ app/views/layouts/guest.blade.php
@@ -12,6 +12,10 @@
         width: 100%;
         height: 100%;
         display: table;
+        background: url('/sunset.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
     }
 
     #container::before {
@@ -21,9 +25,8 @@
         right: 0;
         top: 0;
         bottom: 0;
-        background-color: black;
-        opacity: .1;
-        z-index: -1;
+        background-color: #000;
+        opacity: .3;
     }
 
     #form-signup-container {
@@ -43,6 +46,11 @@
         <div class="col-xs-12 text-center">
             <h1 id="title-banner">Gallery</h1>
         </div>
+        <div class="col-xs-12 text-center">
+            <h3 id="subtitle-banner">Discover The Pretties ;)</h3>
+        </div>
+        <div class="col-xs-12"><br /></div>
+        <div class="col-xs-12"><br /></div>
         <div class="col-xs-12 col-md-4 col-md-offset-4">
             @if (strpos(Route::currentRouteAction(), 'create') !== false)
                 @include('components.signup')
